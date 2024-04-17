bec8f1f4f865909b3cfdd2d5b3ca810fc37194eb
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-17 17:50:39 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+/* global Phaser */
+
+//* Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Nathan De Silva
+// Created on: Apr 20234
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
