588e0f372a6107ce20c8e6bd39fa053fb685296b
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-29 18:20:37 +0000

    edited title scene

--- js/titleScene.js
+++ js/titleScene.js
@@ -1,20 +1,28 @@
 /* global Phaser */
 
-//* Copyright (c) 2024 Nathan De Silva All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
 // Created by: Nathan De Silva
-// Created on: Apr 20234
-// This is the Title Scene 
+// Created on: Apr 2024
+// This is the Title Scene
 
 /**
  * This class is the Title Scene
  */
 class TitleScene extends Phaser.Scene {
   /**
-   * This method is the constructor.
+   * This method is the constructor
    */
   constructor() {
     super({ key: "titleScene" })
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
   }
 
   /**
@@ -24,7 +32,7 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor('ffffff')
   }
 
   /**
@@ -33,25 +41,34 @@ class TitleScene extends Phaser.Scene {
    */
   preload() {
     console.log("Title Scene")
+    this.preload.image("this.titleSceneBackground", "assets/aliens_screen_image.jpg")
   }
 
-  /** 
-   * Can be defined on  your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    //pass
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+
+    this.titleSceneText = this.add
+      .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+      .setOrigin(0.5)
   }
 
   /**
    * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
+   * This method is called once per game step while the scene is running
    * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
+   * @param {number} delta - the delta time in ms since the last frame 
    */
   update(time, delta) {
-    //pass
+    // pass
   }
 }
 
